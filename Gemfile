42e0aaffe555c7c09370b5eb158c27d6360db270
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2019-02-18 16:26:05 +0200

    Rubocop -a

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.1'
 
-gem 'rails', '~> 5.2.2'
+gem 'bootsnap', '>= 1.1.0', require: false
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
-gem 'bootsnap', '>= 1.1.0', require: false
+gem 'rails', '~> 5.2.2'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
@@ -16,9 +18,9 @@ gem 'dotenv-rails'
 gem 'fast_jsonapi'
 
 group :development, :test do
-  gem 'rspec-rails'
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'pry-rails'
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
@@ -28,12 +30,12 @@ group :development do
 end
 
 group :test do
-  gem 'factory_bot_rails'
-  gem 'shoulda-matchers', '4.0.0.rc1'
-  gem 'rails-controller-testing'
-  gem 'faker', git: 'https://github.com/stympy/faker.git', branch: 'master'
   gem 'database_cleaner'
+  gem 'factory_bot_rails'
+  gem 'faker', git: 'https://github.com/stympy/faker.git', branch: 'master'
+  gem 'rails-controller-testing'
+  gem 'shoulda-matchers', '4.0.0.rc1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,38 +12,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_105828) do
-
+ActiveRecord::Schema.define(version: 20_190_218_105_828) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "boards", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'boards', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "cards", force: :cascade do |t|
-    t.string "content"
-    t.text "description"
-    t.integer "position"
-    t.bigint "list_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["list_id", "position"], name: "index_cards_on_list_id_and_position", unique: true
-    t.index ["list_id"], name: "index_cards_on_list_id"
+  create_table 'cards', force: :cascade do |t|
+    t.string 'content'
+    t.text 'description'
+    t.integer 'position'
+    t.bigint 'list_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[list_id position], name: 'index_cards_on_list_id_and_position', unique: true
+    t.index ['list_id'], name: 'index_cards_on_list_id'
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.string "title"
-    t.integer "position"
-    t.bigint "board_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["board_id", "position"], name: "index_lists_on_board_id_and_position", unique: true
-    t.index ["board_id"], name: "index_lists_on_board_id"
+  create_table 'lists', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'position'
+    t.bigint 'board_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[board_id position], name: 'index_lists_on_board_id_and_position', unique: true
+    t.index ['board_id'], name: 'index_lists_on_board_id'
   end
 
-  add_foreign_key "cards", "lists"
-  add_foreign_key "lists", "boards"
+  add_foreign_key 'cards', 'lists'
+  add_foreign_key 'lists', 'boards'
 end
