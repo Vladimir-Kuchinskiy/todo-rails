ee7ad4b6f0faf39a956105745841e5ed3e96498b
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2019-03-26 18:56:12 +0200

    Refactor some code connecter with profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,4 +6,13 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   has_one_attached :avatar
+
+  def avatar_url
+    if avatar.attached?
+      variant = avatar.variant(resize: '200x400')
+      Rails.application.routes.url_helpers.rails_representation_path(variant, only_path: true)
+    else
+      ''
+    end
+  end
 end
--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -5,14 +5,5 @@ class ProfileSerializer
   set_key_transform :camel_lower
   include Rails.application.routes.url_helpers
 
-  attributes :first_name, :last_name, :gender
-
-  attribute :avatar_url do |object|
-    if object.avatar.attached?
-      variant = object.avatar.variant(resize: '200x400')
-      Rails.application.routes.url_helpers.rails_representation_path(variant, only_path: true)
-    else
-      ''
-    end
-  end
+  attributes :first_name, :last_name, :gender, :avatar_url
 end
